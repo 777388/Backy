83d3f1240a3356b26163f8f0d12b11118b727628
777388 <96343159+777388@users.noreply.github.com>
2023-08-22 15:04:35 -0700

    Update background.js

--- background.js
+++ background.js
@@ -6,7 +6,7 @@ chrome.contextMenus.create({
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
   if (info.menuItemId === "backy") {
-    const selectedText = new URL(tab.url);
+    const selectedText = new URL(tab.url).href.replace(/^https?:\/\//, '');
     const archiveUrl = `https://web.archive.org/web/20230000000000*/${selectedText}`
     chrome.tabs.create({ url: archiveUrl });
   }
