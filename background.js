58c650a628f626b96360d845e47cbf72a9bce129
777388 <96343159+777388@users.noreply.github.com>
2023-08-22 14:44:08 -0700

    Add files via upload

---
+++ background.js
@@ -0,0 +1,13 @@
+chrome.contextMenus.create({
+  id: "backy",
+  title: "backy",
+  contexts: ["page"]
+});
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  if (info.menuItemId === "backy") {
+    const selectedText = new URL(tab.url).hostname;
+    const archiveUrl = `https://web.archive.org/web/20230000000000*/${selectedText}`
+    chrome.tabs.create({ url: archiveUrl });
+  }
+});
